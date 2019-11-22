1e041dc7b1704965fb2c3f729c4e8e250f9acf81
Matěj Balga <matej.balga@icloud.com>
2019-11-22 14:07:33 +0100

    Type fixes.

--- types.ts
+++ types.ts
@@ -3,11 +3,11 @@ import { ReactElement } from 'react';
 
 export type Option<F> = F;
 
-export interface InputComponentProps<F, O = undefined> {
+export interface InputComponentProps<T, F, O = undefined> {
   label: string;
   value: F;
   onChange(value: F): void;
-  inputProps: O extends NonNullable<any> ? WithOptionsInputProps<F, O, true> & object : object;
+  inputProps: O extends NonNullable<any> ? WithOptionsInputProps<T, O, true> : object;
 }
 
 export enum SupportedInputs {
@@ -28,13 +28,15 @@ export interface WithOptionsInputProps<T, O, optionsUnwrapped = false> {
   options: optionsUnwrapped extends false ? ((o: T) => Option<O>[]) : Option<O>[];
   optionLabelSelector: (option: O) => NonNullable<React.ReactNode>;
   optionIdentifierSelector: (option: O) => any;
+  [additionalKey: string]: any;
 }
 
-export type ComponentsDictionary<C extends string, F> = Record<C, (props: InputComponentProps<F>) => ReactElement>;
+export type ComponentsDictionary<T, C extends string, F> = Record<C, (props: InputComponentProps<T, F>) => ReactElement>;
 
-export type AutoFormProps<T extends object, F, O, C extends string = SupportedInputs> = {
+export type AutoFormProps<T extends object = any, F = any, O = any, C extends string = SupportedInputs> = {
   o: T;
   fields: Array<Field<T, F, C, O>>;
   updateFn(o: T): void;
-  components?: ComponentsDictionary<C, F>;
-} & object;
+  components?: ComponentsDictionary<T, C, F>;
+  [additionalKey: string]: any
+};
