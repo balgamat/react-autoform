59d306a3aa3e92fc76c8c250aaa26e5fba238a62
Matěj Balga <matej.balga@icloud.com>
2020-10-10 12:46:38 +0200

    v6.1.1

--- src/useAutoform.tsx
+++ src/useAutoform.tsx
@@ -2,6 +2,7 @@ import { ComponentsDictionary, Field, SupportedInputs, ValidationResult } from '
 import React, { FC, useEffect, useState } from 'react';
 import { Autoform } from './Autoform';
 import { createValidationSchema, validation } from './validation';
+import { ValidationError } from 'yup';
 
 export const useAutoform = <T,>(
   onObject: T,
@@ -19,7 +20,7 @@ export const useAutoform = <T,>(
     schema
       .validate(o)
       .then(() => setValidationResult({ valid: true }))
-      .catch(error => setValidationResult({ valid: false, error }));
+      .catch((error: ValidationError) => setValidationResult({ valid: false, error }));
   }, [o]);
 
   const Component: FC = () => <Autoform {...{ o, updateFn, fields: withFields, ...andOptions }} />;
